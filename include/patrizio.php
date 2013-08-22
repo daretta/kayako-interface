f8b6562637f93b5819f8506d9082cbbec3e9b329
dimmipure <dimmipure@gmail.com>
2013-08-22 10:12:58 +0200

    file di configurazione

---
+++ include/patrizio.php
@@ -0,0 +1,12 @@
+<?php
+
+// DB CONFIG
+define(DB_SERVER, '');
+define(DB_USER, '');
+define(DB_PWD, '');
+define(DB_NAME, '');
+
+// KAYAKO CONFIG
+define(KAYAKO_API_SERVER, '');
+define(KAYAKO_API_KEY, '');
+define(KAYAKO_SECRET_KEY, '');
